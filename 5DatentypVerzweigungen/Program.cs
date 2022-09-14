dc2ac2876c9c3151afcea07716e28e960609768a
ErayAlish <111420915+ErayAlish@users.noreply.github.com>
2022-09-14 13:13:00 +0200

    Aufgaben bis 4 erledigt

--- 5DatentypVerzweigungen/Program.cs
+++ 5DatentypVerzweigungen/Program.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 
-//Aufgabe 
+////Aufgabe 
 
 //Console.WriteLine("Biite geben Sie die erste Zahl ein:");
 //string Zahl1 = Console.ReadLine();
@@ -14,33 +14,69 @@
 //{
 //    Console.WriteLine(Zahlint2);
 //}
- 
+
 //if (Zahlint1 < Zahlint2)
 //{
-//    Console.WriteLine(Zahlint1);
-//}
+////    Console.WriteLine(Zahlint1);
+////}
+
+////Aufgabe 3
 
 
-// Aufgabe 2
+//Console.WriteLine("Sind Sie Übergewichtet Bitte beantworten Sie mit true oder false");
+//bool Antwort1 =Convert.ToBoolean(Console.ReadLine());
 
-Console.WriteLine("Bitte geben Sie ein Zahl Zwischen 0-1000");
-string Zahl01 = Console.ReadLine();
-short Zahl;
+//Console.WriteLine("Haben Sie Stress?");
+//bool stress = Convert.ToBoolean(Console.ReadLine());
+//Console.Clear();
+//Console.WriteLine("Die Hertzinfarktrisiko beträgt bei:");
+//if (stress == false && Antwort1 == false)
+//    Console.WriteLine("5%");
+//else if (stress == false && Antwort1 == true)
+//    Console.WriteLine("18%");
+//else if (stress == true && Antwort1 == false)
+//    Console.WriteLine("15%");
+//else if(stress == true && Antwort1 == true)
+//    Console.WriteLine("62%");
 
-Zahl = 500;
 
-int _Zahl01 = Convert.ToInt32(Zahl01);
+////Aufgabe 4  
 
 
-if (Zahl01 < 500)
-{
-    Console.WriteLine("Sie sind zu tief");
-}
+Console.WriteLine("Bitte geben Sie die erste Zahl ein :_");
+string zahl01 = Console.ReadLine();
+Console.WriteLine("Bitte gebén Sie die zweite Zahl ein :_");
+string zahl02 = Console.ReadLine();
 
-else if (Zahl01>500)
+Console.Clear();
+Console.WriteLine("Was möchten sie berechnen");
+Console.WriteLine(@"
+1 Addition 
+2 Subtraktion 
+3 Multiplikation
+4 Division
+5 Potenz
+");
+string was01 = Console.ReadLine();
 
-    {
 
-        Console.WriteLine("Sie sind zu hoch");
+int i = 5; 
+  switch (i)
+  {
+    case 1:
 
-    }
+        if (was01 = 1);
+        do (zahl01 + zahl02 = E1);
+
+    case 2:
+
+    case 3:
+
+    case 4:
+
+    case 5:
+
+  }
+
+  
+
